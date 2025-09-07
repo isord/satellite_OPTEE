9fdff270c2f99ff107226b0f46ff1f5e17d48dde
조민성 <68271769+isord@users.noreply.github.com>
2025-09-07 18:11:08 +0900

    Update kyber_test_ta.h

--- devel/devel/optee/optee_examples/kyber_simple_test/ta/include/kyber_test_ta.h
+++ devel/devel/optee/optee_examples/kyber_simple_test/ta/include/kyber_test_ta.h
@@ -1,5 +1,13 @@
-global-incdirs-y += include
+#ifndef TA_KYBER_TEST_H
+#define TA_KYBER_TEST_H
 
-global-incdirs-y += .
+#define TA_KYBER_TEST_UUID \
+    { 0x8aaaf200, 0x2450, 0x11e4, \
+        { 0xab, 0xe2, 0x00, 0x02, 0xa5, 0xd5, 0xc5, 0x1b} }
 
-srcs-y += kyber_test_ta.c
+#define TA_KYBER_TEST_CMD_KEYGEN    0
+#define TA_KYBER_TEST_CMD_ENCAPS    1
+#define TA_KYBER_TEST_CMD_DECAPS    2
+#define TA_KYBER_TEST_CMD_TEST_DATA 4
+
+#endif
