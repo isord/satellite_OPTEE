79df63693060db207ae9be11be1f326fbbd39783
조민성 <68271769+isord@users.noreply.github.com>
2025-09-07 18:08:44 +0900

    Update user_ta_header_defines.h

--- devel/devel/optee/optee_examples/kyber_simple_test/ta/user_ta_header_defines.h
+++ devel/devel/optee/optee_examples/kyber_simple_test/ta/user_ta_header_defines.h
@@ -1,11 +1,14 @@
 #ifndef USER_TA_HEADER_DEFINES_H
 #define USER_TA_HEADER_DEFINES_H
 
-#define TA_UUID \
+#define TA_KYBER_TEST_UUID \
     { 0x8aaaf200, 0x2450, 0x11e4, \
         { 0xab, 0xe2, 0x00, 0x02, 0xa5, 0xd5, 0xc5, 0x1b} }
 
+#define TA_UUID TA_KYBER_TEST_UUID
+
 #define TA_FLAGS                    TA_FLAG_EXEC_DDR
+
 #define TA_STACK_SIZE               (2 * 1024)
 #define TA_DATA_SIZE                (32 * 1024)
 
